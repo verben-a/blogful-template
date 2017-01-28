e819c62aa14d92d16658615006ba8f5e26732986
Beyonce Knowles <alina.verbenchuk@gmail.com>
2017-01-28 15:49:33 +0000

    Initial commit

---
+++ blog/__init__.py
@@ -0,0 +1,13 @@
+import os
+from . import login
+
+from flask import Flask
+
+app = Flask(__name__)
+config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+app.config.from_object(config_path)
+
+from . import views
+from . import filters
+
+
---
+++ blog/config.py
@@ -0,0 +1,5 @@
+import os
+class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful"
+    DEBUG = True
+    
